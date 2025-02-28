f3a54e1d311f45b70d0b2f8f7959df32f9db94b5
Santosh Bista <santosbista17@gmail.com>
2025-02-28 17:41:16 +0545

    Day-1 of Learning SpringBoot

---
+++ src/test/java/com/santos/bista/myapp/MyappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santos.bista.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
